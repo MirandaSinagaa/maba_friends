03a8aa91122fb5b9ae08ae98ff4da4b1d7db4fb2
MirandaSinagaa <mirandasinaga0803@gmail.com>
2024-07-15 20:43:00 +0800

    Update WelcomeScreen.tsx

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -13,7 +13,7 @@ const WelcomeScreen: React.FC = () => {
     <div className="relative flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-[#0B1E33] to-[#013C58] text-[#f1f5f9] p-4 font-righteous overflow-hidden">
       <div className="flex flex-col items-center justify-center z-10">
         <img src={logo} alt="Logo" className="w-40 h-40 mb-4" /> {/* Disesuaikan ukuran agar pas */}
-        <h1 className="text-4xl font-bold mb-4 text-center">Selamat Datang di Teman Maba!</h1>
+        <h1 className="text-4xl font-bold mb-4 text-center">Selamat Datang di Maba Friends!</h1>
         <p className="text-center mb-8 font-roboto">
           Dapatkan informasi terbaru tentang kampus impianmu!<br />
           Temukan jurusan, beasiswa, dan jalur masuk terbaik.
